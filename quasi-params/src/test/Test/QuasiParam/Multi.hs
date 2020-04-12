cc12bc851f92c725aa9bec59ff6a51995a6768f5
Soares Chen <soares.chen@gmail.com>
2020-04-12 15:30:49 +0800

    Refactor multi param tests

--- quasi-params/src/test/Test/QuasiParam/Multi.hs
+++ quasi-params/src/test/Test/QuasiParam/Multi.hs
@@ -1,17 +1,13 @@
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
-
-module Test.QuasiParam.Multi where
+module Test.QuasiParam.Multi (tests) where
 
 import Test.Tasty
 import Test.Tasty.HUnit
 
 import Data.Kind
-import Data.Coerce
 import GHC.Types (Symbol)
 import Data.Functor.Identity
 
+import Test.QuasiParam.Multi.Item
 import qualified Data.QuasiParam.Multi as Multi
 
 tests :: TestTree
@@ -19,110 +15,20 @@ tests = testGroup "Multi parameters test"
   [ testConversion
   ]
 
-newtype Nil a = Nil ()
-
-newtype Item (name :: Symbol) a = Item
-  { unItem :: a }
-
-newtype Cons e1 e2 a = MkCons
-  { unCons :: (e1 a, e2 a) }
-
-class
-  ( Multi.MultiParam Type (AsMultiParam e)
-  )
-  => IsMultiParam (e :: Type -> Type) where
-    type family AsMultiParam e
-      = (e2 :: Type -> Type) | e2 -> e
-
-    coerceTo :: forall a . e a -> AsMultiParam e a
-    coerceFrom :: forall a . AsMultiParam e a -> e a
-
-instance IsMultiParam Nil where
-  type AsMultiParam Nil = Multi.Empty Type
-
-  coerceTo = coerce
-  coerceFrom = coerce
-
-instance IsMultiParam (Item name) where
-  type AsMultiParam (Item name) = Multi.Elem Symbol Type name Identity
-
-  coerceTo = coerce
-  coerceFrom = coerce
-
-instance
-  ( IsMultiParam e1
-  , IsMultiParam e2
-  , AsMultiParam e1 ~ e1'
-  , AsMultiParam e2 ~ e2'
-  , forall a. Coercible (e1 a) (e1' a)
-  , forall a. Coercible (e1' a) (e1 a)
-  , forall a. Coercible (e2 a) (e2' a)
-  , forall a. Coercible (e2' a) (e2 a)
-  )
-  => IsMultiParam (Cons e1 e2) where
-    type AsMultiParam (Cons e1 e2) =
-      Multi.Cons Type
-        (AsMultiParam e1)
-        (AsMultiParam e2)
-
-    coerceTo = coerce
-    coerceFrom = coerce
-
-withParam
-  :: forall e e' a r
-   . ( IsMultiParam e
-     , AsMultiParam e ~ e'
-     , Multi.MultiParam Type e'
-     , Coercible e e'
-     )
-  => e a
-  -> (Multi.ParamConstraint Type e' a => r)
-  -> r
-withParam e cont = Multi.withParam e' cont
- where
-  e' :: e' a
-  e' = coerce e
-
-captureParam
-  :: forall e e' a
-   . ( IsMultiParam e
-     , AsMultiParam e ~ e'
-     , Multi.MultiParam Type e'
-     , Multi.ParamConstraint Type e' a
-     )
-  => e a
-captureParam = coerceFrom e'
- where
-  e' :: e' a
-  e' = Multi.captureParam @Type @e'
-
-castValue
-  :: forall e1 e2 e1' e2' a
-   . ( IsMultiParam e1
-     , IsMultiParam e2
-     , AsMultiParam e1 ~ e1'
-     , AsMultiParam e2 ~ e2'
-     , Multi.CastParam Type e1' e2'
-     )
-  => e1 a
-  -> e2 a
-castValue e1 = coerceFrom e2'
- where
-  e1' :: e1' a
-  e1' = coerceTo e1
-
-  e2' :: e2' a
-  e2' = Multi.castValue e1'
-
 type Foo = Item "Foo"
 type Bar = Item "Bar"
+type Baz = Item "Baz"
+
 type FooBar = Cons Foo Bar
+type FooBaz = Cons Foo Baz
+type FooBarBaz = Cons Foo (Cons Bar Baz)
+
 type BarFoo = Cons Bar Foo
+type BazBarFoo = Cons Baz (Cons Bar Foo)
 
 type Foo' = Multi.Elem Symbol Type "Foo" Identity
 type Bar' = Multi.Elem Symbol Type "Bar" Identity
 type FooBar' = Multi.Cons Type Foo' Bar'
-type BarFoo' = Multi.Cons Type Bar' Foo'
 
 pattern Foo :: forall a . a -> Item "Foo" a
 pattern Foo a = Item a
@@ -131,6 +37,10 @@ pattern Foo a = Item a
 pattern Bar :: forall a . a -> Item "Bar" a
 pattern Bar a = Item a
 {-# COMPLETE Bar #-}
+
+pattern Baz :: forall a . a -> Item "Baz" a
+pattern Baz a = Item a
+{-# COMPLETE Baz #-}
 
 pattern Cons :: forall e1 e2 a . e1 a -> e2 a -> Cons e1 e2 a
 pattern Cons a b = MkCons (a, b)
@@ -142,8 +52,14 @@ foo = Foo "foo"
 bar :: Bar String
 bar = Bar "bar"
 
+baz :: Baz String
+baz = Baz "baz"
+
 fooBar :: FooBar String
 fooBar = Cons foo bar
+
+fooBarBaz :: FooBarBaz String
+fooBarBaz = Cons foo $ Cons bar baz
 
 foo' :: Foo' String
 foo' = coerceTo foo
@@ -157,11 +73,63 @@ fooBar2 = coerceFrom fooBar'
 barFoo :: BarFoo String
 barFoo = castValue fooBar
 
+bazBarFoo :: BazBarFoo String
+bazBarFoo = castValue fooBarBaz
+
+fooBaz :: FooBaz String
+fooBaz = castValue bazBarFoo
+
 testConversion :: TestTree
 testConversion = testCase "test conversion to multi param" $ do
+  case foo' of
+    Multi.Elem (Identity foo2) ->
+      assertEqual "should be able to convert to canon foo"
+        foo2
+        "foo"
+
+  case fooBar' of
+    Multi.Cons ((Multi.Elem (Identity foo2)), (Multi.Elem (Identity bar2))) ->
+      assertEqual "should be able to convert to canon foo bar"
+        (foo2, bar2)
+        ("foo", "bar")
+
   case barFoo of
     Cons (Bar bar2) (Foo foo2) ->
       assertEqual
         "foo bar should be converted to bar foo"
         (bar2, foo2)
         ("bar", "foo")
+
+  case fooBar2 of
+    Cons (Foo foo2) (Bar bar2) ->
+      assertEqual
+        "foo bar should be converted back to foo bar"
+        (foo2, bar2)
+        ("foo", "bar")
+
+  case fooBaz of
+    Cons (Foo foo2) (Baz baz2) ->
+      assertEqual
+        "foo bar should be converted to foo baz"
+        (foo2, baz2)
+        ("foo", "baz")
+
+  case bazBarFoo of
+    Cons (Baz baz2) (Cons (Bar bar2) (Foo foo2)) ->
+      assertEqual
+        "foo bar should be converted to bar foo"
+        (baz2, (bar2, foo2))
+        ("baz", ("bar", "foo"))
+
+  withParam fooBarBaz $ do
+    assertEqual "should be able to capture foo"
+      (captureParam @Foo)
+      (Foo "foo")
+
+    assertEqual "should be able to capture bar"
+      (captureParam @Bar)
+      (Bar "bar")
+
+    assertEqual "should be able to capture baz"
+      (captureParam @Baz)
+      (Baz "baz")
