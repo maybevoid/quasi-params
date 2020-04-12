7d35a10994767a1568693b43e23e89c656a25007
Soares Chen <soares.chen@gmail.com>
2020-04-12 11:38:19 +0800

    Simplify multi params expressions

--- quasi-params/src/test/Test/QuasiParam/Multi.hs
+++ quasi-params/src/test/Test/QuasiParam/Multi.hs
@@ -1,38 +1,53 @@
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 module Test.QuasiParam.Multi where
 
-import Data.Kind
 import Test.Tasty
 import Test.Tasty.HUnit
 
-import Data.QuasiParam.Multi
+import Data.Kind
+import GHC.Types (Symbol)
+import Data.Functor.Identity
+
+import qualified Data.QuasiParam.Multi as Multi
 
 tests :: TestTree
 tests = testGroup "Multi parameters test"
   [ testCastFooBar
   ]
 
-data Foo
-data Bar
+type Item tag = Multi.Elem Symbol Type tag Identity
+type Union a b = Multi.Union Type (Item a) (Item b)
 
-newtype Value a x = Value { unValue :: a }
-  deriving Eq
+pattern Item :: forall tag t . t -> Item tag t
+pattern Item a = Multi.Elem (Identity a)
 
-type Item tag a = Elem Type Type tag (Value a)
+pattern Union :: forall k1 k2 (label :: k2) (label1 :: k1) t
+  . t -> t
+  -> Multi.Union Type
+      (Multi.Elem k2 Type label Identity)
+      (Multi.Elem k1 Type label1 Identity)
+      t
+pattern Union a b = Multi.Union (Multi.Elem (Identity a)) (Multi.Elem (Identity b))
+{-# COMPLETE Union #-}
 
-fooBar :: Union Type (Item Foo String) (Item Bar String) ()
-fooBar = Union (Elem $ Value "foo") (Elem $ Value "bar")
+foo :: Item "Foo" String
+foo = Item "foo"
 
-barFoo :: Union Type (Item Bar String) (Item Foo String) ()
-barFoo = castValue fooBar
+bar :: Item "Foo" String
+bar = Item "bar"
+
+fooBar :: Union "Foo" "Bar" String
+fooBar = Union "foo" "bar"
+
+barFoo :: Union "Bar" "Foo" String
+barFoo = Multi.castValue fooBar
 
 testCastFooBar :: TestTree
 testCastFooBar = testCase "test cast foo bar" $
   case barFoo of
-    Union (Elem (Value bar)) (Elem (Value foo)) -> do
-      assertEqual "bar should have bar value"
-        bar
-        "bar"
-
-      assertEqual "foo should have foo value"
-        foo
-        "foo"
+    Union bar' foo' -> do
+      assertEqual "foo bar should be casted to bar foo"
+        (bar', foo')
+        ("bar", "foo")
